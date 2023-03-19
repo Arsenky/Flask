ae7a4730864c20bf97dc94ef5f412f5ab214935d
arsenky <arsenky@yandex.ru>
2023-03-19 20:16:53 +0300

    lesson_9_complited

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -1,15 +1,9 @@
 from flask_combo_jsonapi import Api
 from blog.api.tag import TagList, TagDetail
 from combojsonapi.spec import ApiSpecPlugin
-
-
-def init_api(app):
-
-    api = Api(app)
-    api.route(TagList, "tag_list", "/api/tags/")
-    api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/")
-
-    return api
+from blog.api.user import UserList, UserDetail
+from blog.api.author import AuthorList, AuthorDetail
+from blog.api.article import ArticleList, ArticleDetail
 
 def create_api_spec_plugin(app):
     api_spec_plugin = ApiSpecPlugin(
@@ -18,6 +12,9 @@ def create_api_spec_plugin(app):
         # so API gets organized into groups. it's optional.
         tags={
             "Tag": "Tag API",
+            "User": "User API",
+            "Author": "Author API",
+            "Article": "Article API",
         }
     )
     return api_spec_plugin
@@ -34,3 +31,9 @@ def init_api(app):
 
     api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
     api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/", tag="Tag")
+    api.route(UserList, "user_list", "/api/users/", tag="User")
+    api.route(UserDetail, "user_detail", "/api/users/<int:id>/", tag="User")
+    api.route(AuthorList, "author_list", "/api/authors/", tag="Author")
+    api.route(AuthorDetail, "author_detail", "/api/authors/<int:id>/", tag="Author")
+    api.route(ArticleList, "article_list", "/api/articles/", tag="Article")
+    api.route(ArticleDetail, "article_detail", "/api/articles/<int:id>/", tag="Article")
